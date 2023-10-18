1ef2868aeeeb3001d142c1421238624ba0a85ccd
Navay1r0z <n.presnyakov.05@gmail.com>
2023-10-18 16:02:10 +0300

    Dev test file for topic_5

---
+++ topic_5/task3/calc.py
@@ -0,0 +1,38 @@
+from functions import add, subtract, multiply, divide
+
+def main():
+    print("Вітаємо у калькуляторі!")
+    while True:
+        print("Оберіть операцію:")
+        print("1. Додавання")
+        print("2. Віднімання")
+        print("3. Множення")
+        print("4. Ділення")
+        print("5. Вийти")
+        
+        choice = input("Ваш вибір: ")
+        
+        if choice == '5':
+            print("Дякуємо за використання калькулятора. До побачення!")
+            break
+        
+        num1 = float(input("Введіть перше число: "))
+        num2 = float(input("Введіть друге число: "))
+        
+        if choice == '1':
+            result = add(num1, num2)
+            print(f"Результат: {result}")
+        elif choice == '2':
+            result = subtract(num1, num2)
+            print(f"Результат: {result}")
+        elif choice == '3':
+            result = multiply(num1, num2)
+            print(f"Результат: {result}")
+        elif choice == '4':
+            result = divide(num1, num2)
+            print(f"Результат: {result}")
+        else:
+            print("Невірний вибір. Спробуйте ще раз.")
+
+if __name__ == "__main__":
+    main()
